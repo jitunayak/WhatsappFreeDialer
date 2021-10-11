c9bda5a9d4dc2686696bb7dfcdf72cc1f4b4d258
Jitu Nayak <jitunayak715@gmail.com>
2021-10-11 14:20:27 +0530

    1. Can detect whatspp is installed or not 2. Handle empty phone numbers 3. Fixed phone input size issue on android 4. removed some imports

--- src/DialerScreen.js
+++ src/DialerScreen.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { KeyboardAvoidingView, Linking, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import { Alert, KeyboardAvoidingView, Linking, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import PhoneInput from "react-native-phone-number-input";
 import HistoryScreen from './HistoryScreen';
 import { WIDTH } from './utility';
@@ -17,19 +17,31 @@ const DialerScreen = () => {
     const [showMessage, setShowMessage] = useState(false)
     const [countryCode, setCountryCode] = useState('91')
     const [lastUsedNumber, setLastUsedNumber] = useState(null)
+
     const WP_BASE_URL = 'https://wa.me/'
+    const WP_DEEP_LINK = 'whatsapp://send?text=Hi&phone='
+
 
     const openWhatsApp = async () => {
-        let phoneNumber = WP_BASE_URL + countryCode + value;
-        console.log(phoneNumber);
-        await storeAsyncStorageData('LAST_USED_NUMBER', countryCode + value)
-        loadLastPhoneNumber()
-        await Linking.openURL(phoneNumber)
-
+        if (value === '') {
+            Alert.alert('Kindly, enter a phone number');
+        }
+        else {
+            const whatsappInstalled = await Linking.canOpenURL(WP_DEEP_LINK).catch(err => Alert.alert('WhatsApp Not installed'))
+            if (whatsappInstalled) {
+                let phoneNumber = WP_BASE_URL + countryCode + value;
+                await storeAsyncStorageData('LAST_USED_NUMBER', countryCode + value)
+                loadLastPhoneNumber()
+                await Linking.openURL(WP_DEEP_LINK + countryCode + value)
+            }
+        }
     }
 
+
     const openWhatsAppFromLastPhoneNumber = async () => {
-        await Linking.openURL(WP_BASE_URL + lastUsedNumber)
+        Linking.canOpenURL(WP_DEEP_LINK).then(whatsappInstalled => {
+            if (whatsappInstalled) { Linking.openURL(WP_DEEP_LINK + lastUsedNumber) }
+        }).catch(err => Alert.alert('WhatsApp Not installed'))
     }
     async function loadLastPhoneNumber() {
         setLastUsedNumber(await getAsyncStorageData('LAST_USED_NUMBER'))
@@ -40,13 +52,12 @@ const DialerScreen = () => {
     }, [])
 
     return (
-        <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-        >
-
-            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 6 }}      >
-
+        // <KeyboardAvoidingView
+        //     style={{ flex: 1 }}
+        //     behavior={Platform.OS === "ios" ? "padding" : "height"}
+        // >
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}      >
                 <Text style={{ fontSize: 20, fontWeight: '600', marginBottom: 10, color: 'black' }}>WhatsApp Unkown Dialer</Text>
                 <Text style={{ marginHorizontal: 30, marginVertical: 20, fontSize: 14, color: 'gray' }}>{userMsg}</Text>
 
@@ -67,24 +78,24 @@ const DialerScreen = () => {
                     }}
                     withShadow
                     autoFocus={false}
+                    containerStyle={{ width: WIDTH - 20, borderRadius: 6, height: 70 }}
                 />
 
 
-                <TouchableOpacity onPress={openWhatsApp} style={{ backgroundColor: 'green', padding: WIDTH * 0.036, marginTop: 20, borderRadius: 6 }}>
+                <TouchableOpacity onPress={openWhatsApp} style={{ backgroundColor: 'green', padding: WIDTH * 0.026, marginTop: 20, borderRadius: 6 }}>
                     <Text onPress={openWhatsApp} style={{ fontSize: 18, textAlign: 'center', color: 'white', fontWeight: '500' }}>Start chat on Whatsapp</Text>
                 </TouchableOpacity>
                 {
                     lastUsedNumber ? <TouchableOpacity onPress={() => openWhatsAppFromLastPhoneNumber()} style={{
                         backgroundColor: 'wheat', padding: 8, width: WIDTH - 20,
-                        marginTop: 10,
+                        marginTop: 10, borderRadius: 6
                     }}>
-                        <Text onPress={() => openWhatsAppFromLastPhoneNumber()} style={{ fontSize: 18, textAlign: 'center', color: 'black' }}>Last used : +{lastUsedNumber}</Text>
+                        <Text onPress={() => openWhatsAppFromLastPhoneNumber()} style={{ fontSize: 16, textAlign: 'center', color: 'black' }}>Last used : +{lastUsedNumber}</Text>
                     </TouchableOpacity>
                         : <Text>No past record found</Text>
                 }
-            </SafeAreaView>
-        </KeyboardAvoidingView>
-
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
